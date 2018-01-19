b87bafa6e754b1461e20cc398ac4c9e54a35349f
Kristian <krihus05@gmail.com>
2018-01-19 13:55:14 +0100

    database connection

--- RusseSamfunnetServer/src/main/java/com/ntnu/unnamedSoftware/RusseSamfunnetServerApplication.java
+++ RusseSamfunnetServer/src/main/java/com/ntnu/unnamedSoftware/RusseSamfunnetServerApplication.java
@@ -2,8 +2,10 @@ package com.ntnu.unnamedSoftware;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@ImportResource("classpath:app-config.xml")
 public class RusseSamfunnetServerApplication {
 
 	public static void main(String[] args) {
